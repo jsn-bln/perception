e55b7cf4de66d11f198ee5f2ab2c15f0b005cac7
Jayson <jaysonbalano99@gmail.com>
2022-02-03 09:33:27 -0500

    created login route

--- Routes/routes.js
+++ Routes/routes.js
@@ -24,5 +24,28 @@ router.post('/register', (req,res) => {
     })
 })
 
+router.get('/', (req,res) => {
+    Users.find({fullname: req.body.fullname}, (err,user) => {
+        if(err) return res.status(500).json({message: err.message})
+        if(user){
+            return res.status(200).json({ fullname :user.fullname})
+        }
+        if(!user) return res.status(400).json({message: "user not found"})
+    })
+})
+
+router.post('/login', (req,res) => {
+    Users.findOne({username: req.body.username}, async (err, user) => {
+        if(err) return res.status(500).json({message : err.message})
+        if(!user) return res.status(400).json({message: "username not found"})
+        if(user) {
+            const passwordCheck = await bcryt.compare(req.body.password, user.password)
+            console.log(passwordCheck)
+            if(passwordCheck) return res.status(200).json({message: "login successfully"})
+            if(!passwordCheck) return res.status(400).json({message : " incorrect password"})
+        }
+    })
+})
+
 
 module.exports = router
