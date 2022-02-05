c059c3c9c60258305b87b82546dc83ba3ef74dba
Jayson <jaysonbalano99@gmail.com>
2022-02-04 21:11:10 -0500

    fixed error handling on login

--- Routes/routes.js
+++ Routes/routes.js
@@ -37,12 +37,12 @@ router.get('/', (req,res) => {
 router.post('/login', (req,res) => {
     Users.findOne({username: req.body.username}, async (err, user) => {
         if(err) return res.status(500).json({message : err.message})
-        if(!user) return res.status(400).json({message: "username not found"})
+        if(!user) return res.status(400).json({message: "username"})
         if(user) {
             const passwordCheck = await bcryt.compare(req.body.password, user.password)
             console.log(passwordCheck)
             if(passwordCheck) return res.status(200).json({message: "login successfully"})
-            if(!passwordCheck) return res.status(400).json({message : " incorrect password"})
+            if(!passwordCheck) return res.status(400).json({message : "password"})
         }
     })
 })
