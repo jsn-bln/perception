6c090f97f1910411c364ea4603231f60a351203f
Jayson <jaysonbalano99@gmail.com>
2022-02-10 09:46:40 -0500

    homepage boilerplate

---
+++ client/src/components/Homepage/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './homepage.css'
+
+const Homepage = () => {
+  return (
+    <div className='homepage-container'>Homepage</div>
+  )
+}
+
+export default Homepage;
