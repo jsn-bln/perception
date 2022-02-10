16eff126fef67ab8a24c68c0650ae52778c678de
Jayson <jaysonbalano99@gmail.com>
2022-02-10 09:44:58 -0500

    added routes to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import {Route,Routes} from 'react-router-dom'
 import Nav from "./components/Nav/Nav"
+import Login from './components/Login/login';
+import Homepage from './components/Homepage/Homepage'
+import {useSelector,useDispatch} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {actionCreators} from './state/index'
 
 function App() {
 
+  const account = useSelector((state) =>  state)
+  const dispatch = useDispatch()
+  const {userData,userLogin} = bindActionCreators(actionCreators,dispatch)
+  
   return (
     <div className="App">
-        <Nav />
+        <Nav account={account}/>
+        <Routes>
+          <Route exact path='/'element={<Homepage />} />
+          <Route path='/login' element={<Login userdata={userData} userlogin={userLogin} account={account}/>} /> 
+          
+        </Routes>
+        
     </div>
   );
 }
