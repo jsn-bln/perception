e16e5406c23c15eacd428a5d9a7baa57f4b8dc8a
Jayson <jaysonbalano99@gmail.com>
2022-02-15 15:36:56 -0500

    fixed register submit logic

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
 
 
-const Register = () => {
+const Register = (props) => {
 
     const [username, setUsername] = useState('')
     const [firstname, setFirstname] = useState('')
@@ -13,46 +13,81 @@ const Register = () => {
     const [password, setPassword] = useState('')
     const [confirmedPassword, setConfirmedPassword] = useState('')
     const [errorMsg, setErrorMsg] = useState('')
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const payload = {
+            username : username,
+            firstname : firstname,
+            lastname : lastname,
+            password : password,
+            confirmedPassword : confirmedPassword
+        }
+
+        axios({
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: payload,
+            withCredentials: true,
+            url: '/user/register'
+        }).then((res) => {
+            props.userdata(res.data)
+            props.userlogin(true)
+            localStorage.setItem('isLoggedIn', "true")
+            navigate('/')
+        }).catch((err) => {
+            setErrorMsg(err.response.data.message)
+        })
+
+
+
+    }
 
   return (
     <div className='register-container'>
-        <form>
+        <form onSubmit={handleSubmit}>
             <Card className='main-card'>
                 <CardContent className='input-fields'>
+                    <h2>Register</h2>
                     <TextField onChange={(e) => setUsername(e.target.value)} 
                                     className='input-field' 
                                     label="Username" 
                                     variant="outlined" 
                                     error={errorMsg==='username'}
-                                    helperText={errorMsg === "username" ? "Username not found":""}
+                                    helperText={errorMsg === "username" ? "Username is already taken":""}
+                                    required
                                     />
                     <TextField onChange={(e) => setFirstname(e.target.value)} 
                                     className='input-field' 
                                     label="First name" 
                                     variant="outlined" 
-                                    error={errorMsg==='username'}
-                                    helperText={errorMsg === "username" ? "Username not found":""}
+                                    required
                                     />
                     <TextField onChange={(e) => setLastname(e.target.value)} 
                                     className='input-field' 
                                     label="Last name" 
                                     variant="outlined" 
-                                    error={errorMsg==='username'}
-                                    helperText={errorMsg === "username" ? "Username not found":""}
+                                    required
                                     />
                     <TextField onChange={(e) => setPassword(e.target.value)} 
                                     className='input-field' 
                                     label="Password" 
+                                    type='password'
                                     variant="outlined" 
-                                    error={errorMsg==='username'}
-                                    helperText={errorMsg === "username" ? "Username not found":""}
+                                    error={errorMsg==='password'}
+                                    helperText={errorMsg === "password" ? "Password did not match":""}
                                     />
                     <TextField onChange={(e) => setConfirmedPassword(e.target.value)} 
                                     className='input-field' 
                                     label="Confirm Password" 
+                                    type='password'
                                     variant="outlined" 
-                                    error={errorMsg==='username'}
-                                    helperText={errorMsg === "username" ? "Username not found":""}
+                                    error={errorMsg==='password'}
+                                    helperText={errorMsg === "password" ? "Password did not match":""}
                                     />
                     <Button type="submit" className='input-field login-button' variant="contained">Submit</Button>
                 </CardContent>
